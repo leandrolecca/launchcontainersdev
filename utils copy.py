0ed14eae88ea8fe539ac6c09675ad3cc57df06ca
Tiger Yongning Lei <48440236+YongningLei@users.noreply.github.com>
2023-03-06 23:16:33 +0100

    change the utilscopy file

--- utils copy.py
+++ utils copy.py
@@ -5,17 +5,65 @@ import yaml
 from dask import config
 from dask.distributed import Client
 from dask_jobqueue import PBSCluster, SGECluster, SLURMCluster
+LGR = logging.getLogger("GENERAL")
+
+
+def initiate_cluster(jobqueue_yaml, jobs):
+    '''
+    
+
+    Parameters
+    ----------
+    jobqueue_yaml : dictionary
+        read the jobquene_yaml from the yaml file
+    jobs : not clear what should it be
+        basically it's a quene specific thing, needs to check if it's dask specific.
+
+    Returns
+    -------
+    cluster_by_config : dask cluster object
+        according to the jobquene config, we defined a cluster object we want to use.
+
+    '''
+    config.set(distributed__comm__timeouts__tcp="90s")
+    config.set(distributed__comm__timeouts__connect="90s")
+    config.set(scheduler="single-threaded")
+    config.set({"distributed.scheduler.allowed-failures": 50})
+    config.set(admin__tick__limit="3h")
+    
+    if "sge" in jobqueue_yaml["jobqueue"]:
+        cluster_by_config = SGECluster()
+        cluster_by_config.scale(jobs)
+    elif "pbs" in jobqueue_yaml["jobqueue"]:
+        cluster_by_config = PBSCluster()
+        cluster_by_config.scale(jobs)
+    elif "slurm" in jobqueue_yaml["jobqueue"]:
+        cluster_by_config = SLURMCluster()
+        cluster_by_config.scale(jobs)
+    else:
+        LGR.warning(
+            "dask configuration wasn't detected, "
+            "if you are using a cluster please look at "
+            "the jobqueue YAML example, modify it so it works in your cluster "
+            "and add it to ~/.config/dask "
+            "local configuration will be used."
+            "You can find a jobqueue YAML example in the pySPFM/jobqueue.yaml file."
+        )
+        cluster_by_config = None
+    return cluster_by_config
+
 
 def dask_scheduler(jobs):
+    
     """
     Checks if the user has a dask_jobqueue configuration file, and if so,
     returns the appropriate scheduler according to the file parameters
     """
     # look if default ~ .config/dask/jobqueue.yaml exists
     with open(join(expanduser("~"), ".config/dask/jobqueue.yaml"), "r") as stream:
-        data = yaml.load(stream, Loader=yaml.FullLoader)
+        jobqueue_yaml = yaml.load(stream, Loader=yaml.FullLoader)
 
-    if data is None:
+    if jobqueue_yaml is None:
         LGR.warning(
             "dask configuration wasn't detected, "
             "if you are using a cluster please look at "
@@ -26,34 +74,6 @@ def dask_scheduler(jobs):
         )
         cluster = None
     else:
-        cluster = initiate_cluster(data, jobs)
+        cluster = initiate_cluster(jobqueue_yaml, jobs)
     client = None if cluster is None else Client(cluster)
     return client, cluster
-
-
-def initiate_cluster(data, jobs):
-    config.set(distributed__comm__timeouts__tcp="90s")
-    config.set(distributed__comm__timeouts__connect="90s")
-    config.set(scheduler="single-threaded")
-    config.set({"distributed.scheduler.allowed-failures": 50})
-    config.set(admin__tick__limit="3h")
-    if "sge" in data["jobqueue"]:
-        result = SGECluster()
-        result.scale(jobs)
-    elif "pbs" in data["jobqueue"]:
-        result = PBSCluster()
-        result.scale(jobs)
-    elif "slurm" in data["jobqueue"]:
-        result = SLURMCluster()
-        result.scale(jobs)
-    else:
-        LGR.warning(
-            "dask configuration wasn't detected, "
-            "if you are using a cluster please look at "
-            "the jobqueue YAML example, modify it so it works in your cluster "
-            "and add it to ~/.config/dask "
-            "local configuration will be used."
-            "You can find a jobqueue YAML example in the pySPFM/jobqueue.yaml file."
-        )
-        result = None
-    return result
